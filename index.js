ab514aee5f90cd0166e127cf2282db5d120e83f0
Web-Developer-Rakib <rakibodesk7@gmail.com>
2022-12-31 18:55:14 +0600

    Costomer data post function created

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+//DB info
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kobuznu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+// Run DB connection
+const run = async () => {
+  try {
+    // DB client connect
+    await client.connect();
+    //Collections
+    const usersCollection = client.db("crm_support").collection("users");
+    const customersCollection = client
+      .db("crm_support")
+      .collection("customers");
+    app.get("/", (req, res) => {
+      res.send("Hello world");
+    });
+    //Post users info
+    app.post("/post-user", async (req, res) => {
+      const userDetails = req.body;
+      const result = await usersCollection.insertOne(userDetails);
+      res.send(result);
+    });
+    //Post login info
+    app.post("/login", async (req, res) => {
+      try {
+        const username = req.body.username;
+        const password = req.body.password;
+        const matchedUser = await usersCollection.findOne({
+          userName: username,
+        });
+
+        if (matchedUser.password === password) {
+          res.send(matchedUser);
+        } else {
+          res.send({ Invalid: "invalid login details." });
+        }
+      } catch (error) {
+        res.send({ Error: "Something went wrong" });
+      }
+    });
+    //Post users info
+    app.post("/post-customer-details", async (req, res) => {
+      const customerDetails = req.body;
+      const result = await customersCollection.insertOne(customerDetails);
+      res.send(result);
+    });
+  } finally {
+    // Connection continue
+  }
+};
+run().catch(console.dir);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
